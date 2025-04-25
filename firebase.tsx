4fc6575b04a1d03b69f0434fa2e91b4774c6adfa
Ida Krech <krech.ida@gmail.com>
2025-04-25 13:50:49 +0200

    Hide sensitive data

--- firebase.tsx
+++ firebase.tsx
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"
+import Config from "react-native-config";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,12 +11,12 @@ import { getStorage } from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 123,//insert your API key here
-  authDomain: "travel-snap-72662.firebaseapp.com",
-  projectId: "travel-snap-72662",
-  storageBucket: "travel-snap-72662.appspot.com",
-  messagingSenderId: "3116379494",
-  appId: "1:3116379494:web:672aa9d2dd99783cadaca7"
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
